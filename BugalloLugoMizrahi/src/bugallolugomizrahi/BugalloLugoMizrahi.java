04acd1ae2fee982f395950fde6e98049503d2d2f
Ernesto Lugo <ernesto.lugo@correo.unimet.edu.ve>
2022-10-18 21:53:02 -0400

    Disenio de interfaz ventana1,2 y 3

--- BugalloLugoMizrahi/src/bugallolugomizrahi/BugalloLugoMizrahi.java
+++ BugalloLugoMizrahi/src/bugallolugomizrahi/BugalloLugoMizrahi.java
@@ -5,6 +5,8 @@
  */
 package bugallolugomizrahi;
 
+import bugallolugomizrahi.interfaz.Ventana1;
+
 /**
  *
  * @author ernes
@@ -15,23 +17,7 @@ public class BugalloLugoMizrahi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /* Let us create the following graph
-		2 3
-		(0)--(1)--(2)
-		| / \ |
-		6| 8/ \5 |7
-		| /	 \ |
-		(3)-------(4)
-			9		 */
-        MST t = new MST();
-        int graph[][] = new int[][]{{0, 2, 0, 6, 0},
-        {2, 0, 3, 8, 5},
-        {0, 3, 0, 0, 7},
-        {6, 8, 0, 0, 9},
-        {0, 5, 7, 9, 0}};
-
-        // Print the solution
-        t.primMST(graph);
+        Ventana1 v1 = new Ventana1();
     }
 
 }
